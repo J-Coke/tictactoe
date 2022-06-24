efb9414c3c08c725cafdc4cd7fb13b932c6fbc51
James Coker <james.coker@MacBook-Pro.local>
2022-06-24 17:15:24 +0100

    add toggling between players functionality

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -2,17 +2,29 @@ require_relative '../tictactoe'
 
 describe 'grid' do
     new_game = TicTacToe.new
-    it 'returns an array' do
+    it 'is an array' do
         expect(new_game.grid.class).to eq(Array)
     end
-    it 'returns an array of length 3' do
-        expect(new_game.grid.length).to eq(3)
-    end
-    it 'returns an array containing nested arrays of length 3' do
-        expect(new_game.grid[0].length).to eq(3)
+    it 'is an array of length 9' do
+        expect(new_game.grid.length).to eq(9)
     end
     it 'has a 5 in 5 spot and 7 in 7 spot' do
-        expect(new_game.grid[1][1]).to eq 5
-        expect(new_game.grid[2][0]).to eq 7
+        expect(new_game.grid[4]).to eq 5
+        expect(new_game.grid[6]).to eq 7
+    end
+end
+
+describe 'player_turn' do
+    new_game = TicTacToe.new
+    it "begins with player 1's turn" do
+        expect(new_game.player_turn).to eq :player1
+    end
+    it 'toggles to player2 after one move' do
+        new_game.new_move()
+        expect(new_game.player_turn).to eq :player2
+    end
+    it 'toggles back to player1 after two moves' do
+        new_game.new_move()
+        expect(new_game.player_turn).to eq :player1
     end
 end
--- tictactoe.rb
+++ tictactoe.rb
@@ -1,9 +1,18 @@
 class TicTacToe
-    def initialize() 
-        @grid = [[1,2,3], [4,5,6], [7,8,9]]
-    end
-    def players_turn
 
+    attr_reader :grid, :player_turn
+
+    def initialize() 
+        @grid = [1,2,3,4,5,6,7,8,9]
+        @player_turn = :player1
+        # {:player1 => [[1,2,3],]}
     end
-    attr_reader :grid
+
+    def new_move
+        if @player_turn == :player1
+            @player_turn = :player2
+        else
+            @player_turn = :player1
+        end
+    end
 end
