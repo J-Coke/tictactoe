d08674449da17d1fe3d8496542f72f8d5b95539e
James Coker <james.coker@MacBook-Pro.local>
2022-06-29 11:45:17 +0100

    add validator function and print grid function

--- spec/player_move_input_spec.rb
+++ spec/player_move_input_spec.rb
@@ -1,20 +1,25 @@
 require_relative "../tictactoe"
 
-# describe 'grid with moves played' do
-#     new_game = TicTacToe.new
-#     new_game.input(5)
-#     it 'returns grid with :X in central square when player1 inputs 5' do
-#     expect(new_game.grid).to eq [
-#         [1,2,3],
-#         [4,:X,6],
-#         [7,8,9]
-#     ]
-#     end
-# end
-
-describe 'grid with invalid moves played' do
+describe 'grid with moves played' do
     new_game = TicTacToe.new
-    it 'raises an exception when input is not a number between 1-9' do
-        expect(new_game.input('X')).to raise_error("InvalidInput")
+    new_game.grid_updater(5)
+    it 'returns grid with :X in central square when player1 inputs 5' do
+    expect(new_game.grid).to eq [
+        [1,2,3],
+        [4,:X,6],
+        [7,8,9]
+    ]
     end
+end
+
+describe 'validity checker' do
+    new_game = TicTacToe.new
+    it 'displays invalid input message when input is not a number between 1-9' do
+        expect(new_game.input_with_validity_checker('X')).to eq ("Invalid input, please enter a number 1-9")
+    end
+    new_game.grid_updater(5)
+    it 'displays "square already occupied" message when square is occupied' do
+        expect(new_game.input_with_validity_checker(5)).to eq "This square is taken, please choose another"
+    end
+
 end
--- tictactoe.rb
+++ tictactoe.rb
@@ -16,9 +16,8 @@ class TicTacToe
         end
     end
 
-    def input(move)
-        raise "InvalidInput" if move.match(/^[1-9]/)
-        puts move
+    def grid_updater(move)
+        
         @grid.each do | row |
             row.replace(
             row.map do | square |
@@ -27,4 +26,27 @@ class TicTacToe
             )
         end
     end
+
+    def input_with_validity_checker(move)
+        if move.to_s.match?(/[^1-9]/)
+            "Invalid input, please enter a number 1-9"
+        elsif !grid.include?(move)
+            "This square is taken, please choose another"
+        else
+            grid_updater(move)
+        end
+    end
+
+    def print_current_grid
+        print "\n#{grid[0][0]}|#{grid[0][1]}|#{grid[0][2]}\n"
+        print "-----\n"
+        print "#{grid[1][0]}|#{grid[1][1]}|#{grid[1][2]}\n"
+        print "-----\n"
+        print "#{grid[2][0]}|#{grid[2][1]}|#{grid[2][2]}\n"
+    end
+
 end
+
+new_game = TicTacToe.new
+new_game.grid_updater(5)
+new_game.print_current_grid
