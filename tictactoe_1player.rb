81b976b27d9a0764a9b084b8b3f2c086f0a8ade7
Rosie Hamer <rosie.hamer@madetech.com>
2022-07-05 17:41:45 +0100

    Add choose best move with choose winning space and block winning space, update run game

--- tictactoe_1player.rb
+++ tictactoe_1player.rb
@@ -1,9 +1,36 @@
 require_relative './tictactoe'
 
 class TicTacToe1Player < TicTacToe
+
     def random_computer_move
         possible_moves = [1,2,3,4,5,6,7,8,9] - @moves_played.flatten
         possible_moves.sample
+    end
+
+    def choose_winning_space
+        possible_moves = [1,2,3,4,5,6,7,8,9] - @moves_played.flatten
+        nearly_complete_lines = @players_winning_lines[1].select {|line| (line & @moves_played[1]).size == 2}
+        if nearly_complete_lines.length == 1 
+            (nearly_complete_lines[0] & possible_moves)[0]
+        end
+    end
+
+    def block_winning_space
+        possible_moves = [1,2,3,4,5,6,7,8,9] - @moves_played.flatten
+        nearly_complete_lines = @players_winning_lines[0].select {|line| (line & @moves_played[0]).size == 2}
+        if nearly_complete_lines.length >= 1 
+            (nearly_complete_lines[0] & possible_moves)[0]
+        end
+    end
+
+    def choose_best_space
+        if choose_winning_space
+            choose_winning_space
+        elsif block_winning_space
+            block_winning_space
+        else
+            random_computer_move
+        end
     end
 
     def run_game
@@ -13,17 +40,16 @@ class TicTacToe1Player < TicTacToe
             move = input_checker
             new_move(move)
             game_status = winning_lines_check
+            break if game_status != nil
             print_current_grid
-            move = random_computer_move
+            move = choose_best_space
             new_move(move)
             game_status = winning_lines_check
-
         end
         print_current_grid
         print game_status 
     end
+
 end
 
-new_game = TicTacToe1Player.new
-new_game.run_game
  
