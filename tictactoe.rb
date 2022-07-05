d2d6bf920cda23b52e6ad6d7d979d54e09de3e4b
Rosie Hamer <rosie.hamer@madetech.com>
2022-07-05 15:52:36 +0100

    Run game with 1 player, random computer moves

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,4 +1,4 @@
-class TicTacToe
+class TicTacToe 
 
     attr_accessor :grid, :player_turn, :player_symbols, :moves_played, :winning_lines
 
--- tictactoe_1player.rb
+++ tictactoe_1player.rb
@@ -5,7 +5,25 @@ class TicTacToe1Player < TicTacToe
         possible_moves = [1,2,3,4,5,6,7,8,9] - @moves_played.flatten
         possible_moves.sample
     end
+
+    def run_game
+        game_status = nil
+        while game_status == nil do
+            print_current_grid
+            move = input_checker
+            new_move(move)
+            game_status = winning_lines_check
+            print_current_grid
+            move = random_computer_move
+            new_move(move)
+            game_status = winning_lines_check
+
+        end
+        print_current_grid
+        print game_status 
+    end
 end
 
-# new_game = TicTacToe1Player.new
-# new_game.run_game
+new_game = TicTacToe1Player.new
+new_game.run_game
+ 
