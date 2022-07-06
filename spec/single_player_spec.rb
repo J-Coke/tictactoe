067f19ec28a350aed443f93d9dc2d4d80971963d
James Coker <james.coker@madetech.com>
2022-07-06 12:33:15 +0100

    Fix error in block_winning_space, add remove_unavailable_lines method

--- spec/single_player_spec.rb
+++ spec/single_player_spec.rb
@@ -13,7 +13,16 @@ describe 'random_computer_move' do
     end
 end
 
-describe 'choosing a play'
+describe 'remove_unavailable_lines' do
+    new_game = TicTacToe1Player.new
+    it 'removes player1"s winning lines when occupied by player2' do
+        new_game.moves_played = [[1],[]]
+        new_game.remove_unavailable_lines(5)
+        expect(new_game.players_winning_lines[0]).to eq [[1,2,3],[7,8,9],[1,4,7],[3,6,9]]
+    end
+end
+
+describe 'choosing a play' do
 new_game = TicTacToe1Player.new
     describe 'choose_winning_space' do
         it 'chooses to complete a winning line' do
@@ -25,11 +34,22 @@ new_game = TicTacToe1Player.new
         it 'chooses to block the other player when they are about to win' do
             new_game.moves_played = [[1,3],[5]]
             expect(new_game.block_winning_space).to eq 2
+            new_game.moves_played = [[2,5,7],[8,9]]
+            new_game.remove_unavailable_lines(8)
+            new_game.remove_unavailable_lines(9)
+            expect(new_game.block_winning_space).to eq 3
         end
     end
+    # describe 'optimal_non_winning_space' do
+    #     new_game.moves_played = [[5],[]]
+    #     it 'chooses the number contained in the most of their own winning lines' do
+    #         expect([1,3,7,9]).to include(new_game.optimal_non_winning_space)
+    #     end
+    # end
     describe 'choose_best_space' do
         it 'chooses winning space over blocking the other player' do
             new_game.moves_played = [[1,3,9],[5,6]]
             expect(new_game.choose_best_space).to eq 4
         end
     end
+end
--- tictactoe_1player.rb
+++ tictactoe_1player.rb
@@ -18,10 +18,14 @@ class TicTacToe1Player < TicTacToe
     def block_winning_space
         possible_moves = [1,2,3,4,5,6,7,8,9] - @moves_played.flatten
         nearly_complete_lines = @players_winning_lines[0].select {|line| (line & @moves_played[0]).size == 2}
+        print nearly_complete_lines
         if nearly_complete_lines.length >= 1 
             (nearly_complete_lines[0] & possible_moves)[0]
         end
     end
+
+    # def optimal_non_winning_space
+
 
     def choose_best_space
         if choose_winning_space
@@ -33,17 +37,23 @@ class TicTacToe1Player < TicTacToe
         end
     end
 
+    def remove_unavailable_lines(move)
+        @players_winning_lines[@player_turn] = @players_winning_lines[@player_turn].select {|line| !line.include?(move)}
+    end
+
     def run_game
         game_status = nil
         while game_status == nil do
             print_current_grid
             move = input_checker
             new_move(move)
+            remove_unavailable_lines(move)
             game_status = winning_lines_check
             break if game_status != nil
             print_current_grid
             move = choose_best_space
             new_move(move)
+            remove_unavailable_lines(move)
             game_status = winning_lines_check
         end
         print_current_grid
